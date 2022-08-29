54b7807eeb03deddd9a06527b5f27d7e38cb6514
Barinov <Barinov-a-a@mail.ru>
2022-08-29 16:42:20 +0500

    3 коммит 4 задания

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,31 @@ public class Main {
         } else {
             System.out.println("Совершеннолетия ещё не наступил");
         }
+        // Задание 2
+        int kidd = 45;
+        if (kidd <=7) {
+            System.out.println("Ребенок ходит в Школу");
+        }
+        if (kidd >=18)
+        if (kidd <=23) {
+            System.out.println("Отправляться в университет");
+        }
+        if (kidd >=24) {
+            System.out.println("Пора искать первую работу");
+        }
+        // Задание 3
+        int van = 103;
+        if (van <= 59){
+            System.out.println("Остались сидячии и стоячии места");
+        }
+        if (van >=60)
+        if (van <=102) {
+            System.out.println("Остались только стоячие места");
+        }
+        if (van >=103) {
+            System.out.println("Месть нет");
+        }
     }
-    // Задание 2
-    
+
+
 }
